164b019d6159ec5789aa5827976c7cdc39f66a52
TakushiTokuyama <sokoninoruna@gmail.com>
2023-07-30 10:56:24 +0900

    router_Loader

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { Demo } from '../src/pages/Demo'
+import { usersData } from './Data'
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path={'/'} element={<Demo />} errorElement={<div>error</div>}>
+        <Route path={'/'} element={<Demo />} errorElement={<div>error</div>} loader={usersData}>
             <Route path={'demo'} element={<div>demo</div>} />
             <Route path={'demo/:id'} element={<div>demo1</div>} />
         </Route>
